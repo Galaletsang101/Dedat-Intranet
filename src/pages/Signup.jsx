1a2fcf28ba8d8f7471a9cad66c899c8ca01cdde1
Galaletsang101 <morokonyana101@gmail.com>
2026-08-05 10:01:30 +0200

    Fix signup page navigation

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import { Link } from "react-router-dom";
 import AuthLayout from "../layout/AuthLayout";
 import AuthCard from "../components/auth/AuthCard";
 import AuthButton from "../components/auth/AuthButton";
